73f99bf2cdafd1715e9cdf986c6d186eb9973fa0
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-09-28 21:26:55 +0100

    some fixed in main - no longer blocks forever

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,7 +8,7 @@ import cats.syntax.all._
 
 object Main extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
-    val blockingPool: ExecutorService  = Executors.newFixedThreadPool(5)
+    val blockingPool: ExecutorService  = Executors.newFixedThreadPool(1)
     val blocker:      Blocker          = Blocker.liftExecutorService(blockingPool)
     val httpClient:   Client[IO]       = JavaNetClientBuilder[IO](blocker).create
 
@@ -18,12 +18,14 @@ object Main extends IOApp {
     val ffmpegService:           FFMpegServiceAlgebra[IO]           = new FFMpegService[IO]
     val thumbnailCreatorService: ThumbnailCreatorServiceAlgebra[IO] = new ThumbnailCreatorService[IO](ffmpegService, internalArchiveService)
 
+    // there could be some better handling of these arguments
     val videoPath = args(0)
     val time      = args(1).toInt
 
     thumbnailCreatorService.createThumbnail(videoPath, time).flatMap {
-      case Left(err) => IO(println(s"There has been an error: $err")).as(ExitCode.Error) // ideally, there would be a pretty printed version of BusinessError
+      case Left(err) => IO(println(s"There has been an error: $err")).as(ExitCode.Success) // ideally, there would be a pretty printed version of BusinessError
       case Right(()) => IO(println("The thumbnail has been successfully created")).as(ExitCode.Success)
     }
+      .flatTap(_ => IO(blockingPool.shutdown())) //don't forget to shutdown the pool
   }
 }
